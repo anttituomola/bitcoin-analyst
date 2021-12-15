04891b61d2a264898b5f30ceda34a03c4a56bb0c
Antti <anttituomola8@gmail.com>
2021-12-15 11:58:43 +0200

    fetch data from Gecko API

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+document.getElementById("submitBtn").addEventListener("click", handleSubmit)
 const startDateEl = document.getElementById("startDateEl")
 const endDateEl = document.getElementById("endDateEl")
-document.getElementById("submitBtn").addEventListener("click", submit)
 
-function submit() {
-    console.log(startDateEl.value, endDateEl)
+function handleSubmit() {
+    const startDate = new Date(startDateEl.value)
+    const startDateTimestamp = Date.parse(startDate)/1000
+    const endDate = new Date(endDateEl.value)
+    const endDateTimestamp = Date.parse(endDate)/1000
+    console.log(startDateTimestamp, endDateTimestamp)
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${startDateTimestamp}&to=${endDateTimestamp}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
