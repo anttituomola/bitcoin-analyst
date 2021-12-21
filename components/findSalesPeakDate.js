0fb7c4d645158ed2e67837708cb20fd0d2f27321
Antti <anttituomola8@gmail.com>
2021-12-21 09:28:08 +0200

    new folders

---
+++ components/findSalesPeakDate.js
@@ -0,0 +1,17 @@
+const salesPeakEl = document.getElementById("salesPeakEl")
+
+//FIND TRANSACTION PEAK DATE
+export default function findSalesPeakDate(data) {
+    const firstHourDatapoints = data
+    //sort dates by value + grab the highest
+    const sortedIntances = firstHourDatapoints.sort((a, b) => b.value - a.value)
+    const highestVolumeDay = sortedIntances[0]
+
+    //render the results to the app
+    salesPeakEl.innerHTML = `
+    <h3>Highest trading volume:</h3>
+    <h1>${dayjs.utc(highestVolumeDay.timestamp).format("DD.MM.YYYY")}</h1>
+    <h3>Total trading volume:</h3>
+    <p>${Math.round(highestVolumeDay.value)} €</p>
+    `
+}
