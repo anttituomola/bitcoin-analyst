5f2215c481797147cae8a18884b9910028b49a45
Antti <anttituomola8@gmail.com>
2021-12-28 20:58:41 +0200

    render method on sales peak

--- components/findSalesPeakDate.js
+++ components/findSalesPeakDate.js
@@ -2,20 +2,27 @@
 
 //FIND TRANSACTION PEAK DATE
 export default class FindSalesPeakDate {
+    constructor() {
+        this.render = this.render.bind(this)
+    }
     static findSalesPeakDate(data) {
         const firstHourDatapoints = data
         //sort dates by value + grab the highest
         const sortedIntances = firstHourDatapoints.sort((a, b) => b.value - a.value)
         const highestVolumeDay = sortedIntances[0]
-        
-        //render the results to the app
-        const salesPeakEl = document.getElementById("salesPeakEl")
-        salesPeakEl.innerHTML = `
-        <h3>Highest trading volume:</h3>
-        <h1>${dayjs.utc(highestVolumeDay.timestamp).format("DD.MM.YYYY")}</h1>
-        <p>On that day:</p>
-        <p>${Math.round(highestVolumeDay.value)} €</p>
-        `
+
+        this.render(highestVolumeDay)
     }
 
+    //render the results to the app
+    static render(highestVolumeDay) {
+        const salesPeakEl = document.getElementById("salesPeakEl")
+        salesPeakEl.innerHTML = `
+            <h3>Highest trading volume:</h3>
+            <h1>${dayjs.utc(highestVolumeDay.timestamp).format("DD.MM.YYYY")}</h1>
+            <p>On that day:</p>
+            <p>${Math.round(highestVolumeDay.value)} €</p>
+            `
+    }
 }
+
